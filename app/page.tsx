3c82654ba8a3fc0c33e8535e39337449d84cdf08
Janos Lukacs <pszichae@gmail.com>
2023-11-22 21:33:23 +0100

    minor design adjustment from figma

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import PasswordGenerator from '@/components/password-generator';
 
 export default function Home() {
   return (
-    <div className='flex min-h-screen w-full items-start justify-center bg-gray-darkest px-4 py-16 md:py-[96px]'>
+    <div className='flex min-h-screen w-full items-start justify-center bg-[#08070b] px-4 py-16 md:py-[133px]'>
       <PasswordGenerator />
     </div>
   );
