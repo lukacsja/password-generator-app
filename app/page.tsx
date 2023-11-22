db73edfe22b36d3973e547dbb6da1595ffccc71b
Janos Lukacs <pszichae@gmail.com>
2023-11-22 21:26:37 +0100

    removed extra <main> tag

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import PasswordGenerator from '@/components/password-generator';
 
 export default function Home() {
   return (
-    <main className='flex min-h-screen w-full items-start justify-center bg-gray-darkest px-4 py-16 md:py-[96px]'>
+    <div className='flex min-h-screen w-full items-start justify-center bg-gray-darkest px-4 py-16 md:py-[96px]'>
       <PasswordGenerator />
-    </main>
+    </div>
   );
 }
